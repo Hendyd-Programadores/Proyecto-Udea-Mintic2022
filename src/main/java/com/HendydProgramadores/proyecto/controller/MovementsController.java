a2bd3a23a841ecfbe45db8eccc89d4cf64093d02
Nicol Palacios <nicolpalacios86@gmail.com>
2022-09-13 23:38:44 -0500

    Movements Controller

--- src/main/java/com/HendydProgramadores/proyecto/controller/MovementsController.java
+++ src/main/java/com/HendydProgramadores/proyecto/controller/MovementsController.java
@@ -1,9 +1,33 @@
 package com.HendydProgramadores.proyecto.controller;
 
+import com.HendydProgramadores.proyecto.model.Empresa;
+import com.HendydProgramadores.proyecto.model.MovimientoDinero;
+import com.HendydProgramadores.proyecto.repository.MovimientoDineroRepository;
+import com.HendydProgramadores.proyecto.service.EnterpriseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MovementsController {
 
+    @Autowired
+    private MovimientoDineroRepository repository;
+
+    @GetMapping("/enterprises/[id]/movements")
+    private void getByEnterprise(@PathVariable Long id) {
+        repository.findAll();
+    }
+    @PostMapping("/enterprises/[id]/movements")
+    private void saveByEnterprise(@PathVariable  Long id, @RequestBody MovimientoDinero transaccion) {
+        repository.saveByEnterprise(id, transaccion);
+    }
+    @PatchMapping("/enterprises/[id]/movements")
+    private void patchByEnterprise(@PathVariable Long id, @RequestBody MovimientoDinero movimientoDinero){
+        repository.patchByEnterprise(id, movimientoDinero);
+    }
+
+    @DeleteMapping("/enterprises/[id]/movements")
+    private void deleteByEnterprise(@PathVariable("id") Long id){
+        repository.deleteByEnterprise(id);
+    }
 }
--- src/main/java/com/HendydProgramadores/proyecto/repository/MovimientoDineroRepository.java
+++ src/main/java/com/HendydProgramadores/proyecto/repository/MovimientoDineroRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Repository
 public interface MovimientoDineroRepository<T> extends JpaRepository<MovimientoDinero, Long> {
+
     //GET
     @Query(value = "select * from Empresa", nativeQuery = true)
     public abstract List<MovimientoDinero> findAll();
@@ -19,10 +20,12 @@ public interface MovimientoDineroRepository<T> extends JpaRepository<MovimientoD
     public  abstract boolean saveByEnterprise(long id, MovimientoDinero transaccion);
     @Query(value = "insert into transacciones (id,concepto,monto,id_empresa,id_empleado) values (?1,?2,?3,?4,?5)", nativeQuery = true)
     public abstract void querySaveMovimientoDinero(long id, String concepto, float monto, long id_empresa, long id_empleado);
+
     //PATCH
     public abstract MovimientoDinero patchByEnterprise(long id, MovimientoDinero movimientoDinero);
     @Query(value = "update transacciones set id = ?1,concepto=?2,monto=?3,id_empresa=?4,id_empleado=?5 where id_empresa=?6", nativeQuery = true)
     public abstract void queryPatchTransaction(long id,String concepto,float monto,long id_new_empresa,long id_empleado,long id_empresa );
+
     //DELETE
     @Query(value = "delete from transacciones where id_empresa=?1",nativeQuery = true)
     public abstract boolean deleteByEnterprise(long id);
