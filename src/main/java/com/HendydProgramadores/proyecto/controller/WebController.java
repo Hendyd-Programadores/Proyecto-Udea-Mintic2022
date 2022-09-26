91e5390733b303ded097a2e743a8a2b3e3947afc
Nicol Palacios <nicolpalacios86@gmail.com>
2022-09-25 20:17:33 -0500

    Dependencia Thymeleaf, Web Controller, index.html y estilos.css

---
+++ src/main/java/com/HendydProgramadores/proyecto/controller/WebController.java
@@ -0,0 +1,21 @@
+package com.HendydProgramadores.proyecto.controller;
+import com.HendydProgramadores.proyecto.service.EmployedService;
+import com.HendydProgramadores.proyecto.service.EnterpriseService;
+import com.HendydProgramadores.proyecto.service.MovimientoDineroService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebController {
+
+    @GetMapping()
+    public String index (){
+        return "index";
+    }
+
+
+
+
+
+}
